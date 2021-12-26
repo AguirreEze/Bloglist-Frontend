e7a4ed066c1d741990f36f3082e08752b178cd5b
AguirreEze <aguirre.eze95@gmail.com>
2021-12-26 16:19:30 -0300

    Feat test v2

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,24 +1,37 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Blog from './Blog'
 
-test('Initial component renders Blogs title and author, but dosent render url and likes', () => {
-  const blog = {
-    title: 'Testing React',
-    author: 'Ezequiel',
-    url: 'https://www.google.com',
-    likes: 1,
-    user: 'Tydrok',
-    id: 3256481354
-  }
+const blog = {
+  title: 'Testing React',
+  author: 'Ezequiel',
+  url: 'https://www.google.com',
+  likes: 1,
+  user: 'Tydrok',
+  id: 3256481354
+}
+let component
 
-  const component = render(<Blog blog={blog} />)
+describe('<Blog /> tests', () => {
+  beforeEach(() => {
+    component = render(<Blog blog={blog} />)
+  })
 
-  component.getByText(blog.title)
-  component.getByText(blog.author)
-  const likes = component.queryByText('likes')
-  expect(likes).toBe(null)
-  const url = component.queryByText(blog.url)
-  expect(url).toBe(null)
+  test('Initial component renders Blogs title and author, but dosent render url and likes', () => {
+    component.getByText(blog.title)
+    component.getByText(blog.author)
+    const likes = component.queryByText(`likes: ${blog.likes}`)
+    expect(likes).toBe(null)
+    const url = component.queryByText(blog.url)
+    expect(url).toBe(null)
+  })
+
+  test.skip('When unfolding Blog shows url and Likes', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    component.getByText(blog.url)
+    component.getByText(`likes: ${blog.likes}`)
+  })
 })
